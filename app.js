e2814f0c9a0b6ac185dd026e7b036c572352529c
e_hasitha <e.hasitha@directfn.com>
2025-02-08 21:46:45 +0300

    Mongoose Implementation

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database").mongoConnect;
+const mongoose = require("mongoose");
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const { log } = require("console");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
-    User.findById("678e8d30255b3b330cc47497").then((user) => {
-        req.user = user;
-        next();
-    });
+  User.findById("678e8d30255b3b330cc47497").then((user) => {
+    req.user = user;
+    next();
+  });
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404Page);
 
-mongoConnect(() => {
+mongoose
+  .connect(
+    "mongodb+srv://read_write_user:iUadHdjj9dOwpQBt@cluster0.w7yvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then((result) => {
     app.listen(3000);
-});
-
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- routes/admin.js
+++ routes/admin.js
@@ -4,14 +4,14 @@ const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
-// /admin/add-product => GET
-router.get("/add-product", adminController.getAddProducts);
-router.get("/products", adminController.getProducts);
-router.get("/edit-product/:productId", adminController.getEditProducts);
+// // /admin/add-product => GET
+// router.get("/add-product", adminController.getAddProducts);
+// router.get("/products", adminController.getProducts);
+// router.get("/edit-product/:productId", adminController.getEditProducts);
 
-// /admin/add-product => POST
-router.post("/add-product", adminController.postAddProducts);
-router.post("/edit-product", adminController.postEditProducts);
-router.post("/delete-product", adminController.postDeleteProducts);
+// // /admin/add-product => POST
+// router.post("/add-product", adminController.postAddProducts);
+// router.post("/edit-product", adminController.postEditProducts);
+// router.post("/delete-product", adminController.postDeleteProducts);
 
 module.exports = router;
