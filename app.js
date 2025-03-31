73db2e0d2318c5cc544594e6f203e6f078212fe3
Hasitha Edirisinghe <hsedirisingha@gmail.com>
2025-04-01 00:04:18 +0530

    Fixed the populate method issue

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.get404Page);
+app.use(errorController.get404);
 
 mongoose
   .connect(
