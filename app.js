b799d2ee4a7a3b29e8baef8e88b883e291fee1c5
Hasitha Edirisinghe <hsedirisingha@gmail.com>
2025-04-07 20:53:32 +0530

    Add auth view

--- app.js
+++ app.js
@@ -13,41 +13,41 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const { log } = require("console");
+const authRoutes = require("./routes/auth");
+const {log} = require("console");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
-  User.findById("67c1cf6c03cacef2faf6da18").then((user) => {
-    req.user = user;
-    next();
-  });
+    User.findById("67c1cf6c03cacef2faf6da18").then((user) => {
+        req.user = user;
+        next();
+    });
 });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://read_write_user:iUadHdjj9dOwpQBt@cluster0.w7yvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        user = new User({
-          name: "Hasitha",
-          email: "hse@y.com",
-          cart: {
-            items: [],
-          },
+    .connect("mongodb+srv://read_write_user:iUadHdjj9dOwpQBt@cluster0.w7yvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    .then((result) => {
+        User.findOne().then((user) => {
+            if (!user) {
+                user = new User({
+                                    name: "Hasitha",
+                                    email: "hse@y.com",
+                                    cart: {
+                                        items: []
+                                    }
+                                });
+                user.save();
+            }
         });
-        user.save();
-      }
+        app.listen(3000);
+    })
+    .catch((err) => {
+        console.log(err);
     });
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
